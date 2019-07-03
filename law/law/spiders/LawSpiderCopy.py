55b1916c472b74108dcad237bcd958113eff4285
Zixh <502246880@qq.com>
2019-07-03 10:01:19 +0800

    若干改动

--- law/law/spiders/LawSpiderCopy.py
+++ law/law/spiders/LawSpiderCopy.py
@@ -2,7 +2,6 @@ import os
 import scrapy
 from scrapy.http import Request
 import requests
-from scrapy.exceptions import NotSupported
 from scrapy.utils.project import get_project_settings
 import pymysql
 from lxml import etree
@@ -37,7 +36,7 @@ class LawSpider(scrapy.Spider):
 
     def get_urls(self, response):
         row = response.meta['row']
-        urls = response.xpath(row['target_url_xpath']).extract()    #提取页面条目列表url
+        urls = response.xpath(row['target_url_xpath']).extract()    #提取页面列表url
         # print(urls)
         for url in urls:
             url = response.urljoin(url)
@@ -46,7 +45,7 @@ class LawSpider(scrapy.Spider):
             except Exception as e:
                 print(Exception, ":", e)
 
-        print(response.xpath(row['nextpage_url_xpath']).extract()[0])
+        # print(response.xpath(row['nextpage_url_xpath']).extract()[0])
         next_page = response.xpath(row['nextpage_url_xpath']).extract()[0]
         if next_page != row['nextpage_end_condition']:
             print('nextpage---------' + next_page)
@@ -56,7 +55,6 @@ class LawSpider(scrapy.Spider):
     # 下载html/pdf
     def download_html(self, response):
         row = response.meta['row']
-
         file_path = "download/HTML/"+str(row['title'])+"/"
         if not os.path.exists(file_path):
             os.makedirs(file_path)
@@ -67,12 +65,11 @@ class LawSpider(scrapy.Spider):
             print("-------------------------------------")
             print("已保存HTML : "+html_name)
             print("-------------------------------------")
-
             try:
+                self.get_appendix(response)
                 self.html_parser(row, file_path + html_name + ".html")
             except Exception as e:
                 print(Exception, ":", e)
-
         except:
             url = response.url
             file_name = url.split('/')[-1]
@@ -98,25 +95,18 @@ class LawSpider(scrapy.Spider):
             doc = selector.xpath(row['doc_div_xpath'])
             self.store_data(title, doc, source)
 
-    def get_info(self, response):
-        url = response.url
+    # 查找附件
+    def get_appendix(self, response):
         row = response.meta['row']
         try:
-            # 处理附件
             urls = response.xpath(row['appendix_xpath']).extract()
             for file_url in urls:
                 print('处理附件......')
                 file_url = response.urljoin(file_url)
                 print('附件URL:'+file_url)
                 self.download_file(response, file_url)
-
-        except NotSupported as e:
-            # 处理html
-            print('处理HTML......')
-            self.download_html(response)
-            print('HTML : ' + url)
-        except:
-            pass
+        except Exception as e:
+            print("查找附件时异常：" + str(e))
 
     # 下载附件
     def download_file(self, response, url):
@@ -138,7 +128,7 @@ class LawSpider(scrapy.Spider):
                     f.write(chunk)
             print('保存文件: %s' % file_name)
         except:
-            print('error')
+            pass
 
     # 将数据传递至数据库的web_lawnature表
     def store_data(self, title, doc, source):
@@ -151,6 +141,6 @@ class LawSpider(scrapy.Spider):
             self.cursor.connection.commit()
             print("---------------新增一条数据----------------")
         except Exception as e:
-            print("出现异常：" + str(e))
+            print("数据存库异常：" + str(e))
 
 
